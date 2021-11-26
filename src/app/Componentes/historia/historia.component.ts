87d72ea9e891a13625a610f1a13d0f056d86cb6a
FrikiMeloManiaco <94015973+FrikiMeloManiaco@users.noreply.github.com>
2021-11-26 17:01:51 -0600

    Funciono you tube desde el software

--- src/app/Componentes/historia/historia.component.ts
+++ src/app/Componentes/historia/historia.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { formatDistance } from 'date-fns';
+//import { formatDistance } from 'date-fns';
 
 @Component({
   selector: 'app-historia',
@@ -13,33 +13,33 @@ export class HistoriaComponent implements OnInit {
   ngOnInit() {
   }
 
-  data: any[] = [];
-  submitting = false;
-  user = {
-    author: 'Han Solo',
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
-  };
-  inputValue = '';
-
-  handleSubmit(): void {
-    this.submitting = true;
-    const content = this.inputValue;
-    this.inputValue = '';
-    setTimeout(() => {
-      this.submitting = false;
-      this.data = [
-        ...this.data,
-        {
-          ...this.user,
-          content,
-          datetime: new Date(),
-          displayTime: formatDistance(new Date(), new Date())
-        }
-      ].map(e => ({
-        ...e,
-        displayTime: formatDistance(new Date(), e.datetime)
-      }));
-    }, 800);
-  }
+//  data: any[] = [];
+//  submitting = false;
+//  user = {
+//    author: 'Han Solo',
+//    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+//  };
+//  inputValue = '';
+//
+//  handleSubmit(): void {
+//    this.submitting = true;
+//    const content = this.inputValue;
+//    this.inputValue = '';
+//    setTimeout(() => {
+//      this.submitting = false;
+//      this.data = [
+//        ...this.data,
+//        {
+//          ...this.user,
+//          content,
+//          datetime: new Date(),
+//          displayTime: formatDistance(new Date(), new Date())
+//        }
+//      ].map(e => ({
+//        ...e,
+//        displayTime: formatDistance(new Date(), e.datetime)
+//      }));
+//    }, 800);
+//  }
 
 }
